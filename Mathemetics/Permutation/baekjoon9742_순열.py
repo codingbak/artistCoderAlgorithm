d1768cbb578f1c4d5a60b30e19c54bfae912b567
artistCoder <reformer210@naver.com>
2022-03-23 22:44:35 +0900

    순열문제 해결 배운점 : idx생각 좀 더 깊게 해보기, factorial ''.join

--- Mathemetics/Permutation/baekjoon9742_순열.py
+++ Mathemetics/Permutation/baekjoon9742_순열.py
@@ -1,15 +1,29 @@
 ##permutation 써서 푸는 문제가 아니였다....
 
 from itertools import permutations
+import math
 
 while(1):
     try:
-        s, idx = map(str, input().split())
+        s, stringIdx = map(str, input().split())
     except:
         break
 
-    try:
-        result = ''.join(list(permutations(s))[int(idx) - 1])
-        print("{} {} = {}".format(s, idx, result))
-    except:
-        print("{} {} = No permutation".format(s, idx))
+    idx = int(stringIdx)
+    sLen = len(s)
+    sLenFactorial= math.factorial(sLen)
+    result = []
+    sList = list(s)
+    try :
+        while sLen != 0:
+            div = sLenFactorial/sLen
+            result += sList[int((idx-1)/div)]
+            sList.pop(int((idx-1)/div))
+            idx -= int((idx-1)/div)*div
+            sLen -= 1
+            sLenFactorial = div
+        print("{} {} = {}".format(s, stringIdx, ''.join(result)))
+    except :
+        print("{} {} = No permutation".format(s, stringIdx))
+
+
